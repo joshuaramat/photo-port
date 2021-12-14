e9255b9999e1399fa83e2b20ef3abd7653cc54ae
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2021-12-13 18:55:49 -0800

    added testing suite to ContactForm component and updated snapshots of the other testing suites

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -18,6 +20,8 @@ describe('Nav component', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            SetContactSelected={mockSetContactSelected}
         />);
     });
 
@@ -28,6 +32,8 @@ describe('Nav component', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            SetContactSelected={mockSetContactSelected}
         />);
         // assert value comparison
         expect(asFragment()).toMatchSnapshot();
@@ -42,6 +48,8 @@ describe('emoji is visible', () => {
                 categories={categories}
                 setCurrentCategory={mockSetCurrentCategory}
                 currentCategory={mockCurrentCategory}
+                contactSelected={mockContactSelected}
+                SetContactSelected={mockSetContactSelected}
             />);
         // Assert
         expect(getByLabelText('camera')).toHaveTextContent('📸');
@@ -56,6 +64,8 @@ describe('links are visible', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            SetContactSelected={mockSetContactSelected}
         />);
         // Assert
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
